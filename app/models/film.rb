afbd270b083c993319bfaf2721657c82433b8db0
Bastien Vaucher <bastien.vaucher@gmail.com>
2011-03-22 23:15:09 +0100

    preventing infinite loop

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,11 +3,12 @@ require 'nokogiri'
 
 class Film < ActiveRecord::Base
   attr_accessible :imdb_url
+  attr_accessible :skip_imdb_url
   
   after_save :get_imdb_data
   
   def get_imdb_data
-    if imdb_url_changed?
+    if imdb_url_changed? && !@skip_imdb_url
       puts "[imdb_url] changed"
       update_imdb_data
     end
@@ -17,6 +18,7 @@ class Film < ActiveRecord::Base
     doc = Nokogiri::HTML(open(imdb_url))
     rating = doc.css(".rating-rating").first.content.split("/").first.to_f
     puts "rating : #{rating}"
+    @skip_imdb_url = true
     update_attribute(:imdb_rating, rating)
   end
 end
